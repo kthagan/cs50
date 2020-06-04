bb8bfeb7a419c509dd1324f831fb996cd4a6a166
kthagan <kthagan@yahoo.com>
2020-06-04 12:16:57 -1000

    randomize the gap between pipes between 70 and 120 rather than fixed at 90

--- assignment1.dad/PipePair.lua
+++ assignment1.dad/PipePair.lua
@@ -10,9 +10,6 @@
 
 PipePair = Class{}
 
--- size of the gap between pipes
-local GAP_HEIGHT = 90
-
 function PipePair:init(y)
     -- flag to hold whether this pair has been scored (jumped through)
     self.scored = false
@@ -23,10 +20,15 @@ function PipePair:init(y)
     -- y value is for the topmost pipe; gap is a vertical shift of the second lower pipe
     self.y = y
 
+    -- size of the gap between pipes -- KTH HERE for gap between pipes
+    local gapHeight= 90
+	gapHeight = math.random(70, 120)
+
+
     -- instantiate two pipes that belong to this pair
     self.pipes = {
         ['upper'] = Pipe('top', self.y),
-        ['lower'] = Pipe('bottom', self.y + PIPE_HEIGHT + GAP_HEIGHT)
+        ['lower'] = Pipe('bottom', self.y + PIPE_HEIGHT + gapHeight)
     }
 
     -- whether this pipe pair is ready to be removed from the scene
