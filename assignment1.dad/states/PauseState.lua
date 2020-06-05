e58adf1f7521d911ab50d2e827a7e79b3a354de1
kthagan <kthagan@yahoo.com>
2020-06-04 16:29:03 -1000

    clean up this file even though I am not going to use it

--- assignment1.dad/states/PauseState.lua
+++ assignment1.dad/states/PauseState.lua
@@ -1,8 +1,6 @@
 PauseState = Class{__includes = BaseState}
 
 function PauseState:init()
-    self.count = 2
-    self.timer = 0
 end
 
 --[[
@@ -11,26 +9,14 @@ end
     we should transition to our PlayState.
 ]]
 function PauseState:update(dt)
-        -- when 0 is reached, we should enter the PlayState
-        if self.count == 0 then
-            gStateMachine:change('play')
-        end
-
---[[
-    self.timer = self.timer + dt
-
-    -- loop timer back to 0 (plus however far past COUNTDOWN_TIME we've gone)
-    -- and decrement the counter once we've gone past the countdown time
-    if self.timer > COUNTDOWN_TIME then
-        self.timer = self.timer % COUNTDOWN_TIME
-        self.count = self.count - 1
-
+    -- transition to play when 'p' is pressed
+    if love.keyboard.wasPressed('p') then
+        gStateMachine:change('play')
     end
-]]
 end
 
 function PauseState:render()
-    -- render count big in the middle of the screen
+    -- render pause spause string big in the middle of the screen
     love.graphics.setFont(hugeFont)
     love.graphics.printf("||", 0, 120, VIRTUAL_WIDTH, 'center')
 end
