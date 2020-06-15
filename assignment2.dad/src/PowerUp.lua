c488960f38d834d987d266495c4ffbb901746641
kthagan <kthagan@yahoo.com>
2020-06-14 20:41:14 -1000

    get rid of unused code for dx and wall bounce

--- assignment2.dad/src/PowerUp.lua
+++ assignment2.dad/src/PowerUp.lua
@@ -8,7 +8,6 @@ function PowerUp:init()
     -- set initial x, y and velocities
     self.x = math.random(self.width + 5, VIRTUAL_WIDTH - self.width - 5)
     self.y = self.height + 10 -- start near top of screen
-    self.dx = 0
     self.dy = 25  -- 25 seems to be a nice fall rate; may need to take into account frame rate
 end
 
@@ -40,32 +39,11 @@ KTH - I think we will delete this function
 function PowerUp:reset()
     self.x = VIRTUAL_WIDTH / 2 - 2
     self.y = VIRTUAL_HEIGHT / 2 - 2
-    self.dx = 0
     self.dy = 0
 end
 
 function PowerUp:update(dt)
-    self.x = self.x + self.dx * dt
     self.y = self.y + self.dy * dt
-
-    -- allow ball to bounce off walls
-    if self.x <= 0 then
-        self.x = 0
-        self.dx = -self.dx
-        gSounds['wall-hit']:play()
-    end
-
-    if self.x >= VIRTUAL_WIDTH - 8 then
-        self.x = VIRTUAL_WIDTH - 8
-        self.dx = -self.dx
-        gSounds['wall-hit']:play()
-    end
-
-    if self.y <= 0 then
-        self.y = 0
-        self.dy = -self.dy
-        gSounds['wall-hit']:play()
-    end
 end
 
 function PowerUp:render()
